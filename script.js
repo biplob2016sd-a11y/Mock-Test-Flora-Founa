f98bf9dcf2379545c635f61809404ca0eeadb512
biplob2016sd-a11y <biplob2016sd@gmail.com>
2026-07-13 21:21:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,11 +4,15 @@ document.getElementById("startBtn").addEventListener("click", function () {
     let email = document.getElementById("email").value.trim();
     let mobile = document.getElementById("mobile").value.trim();
 
-    if(name=="" || email=="" || mobile==""){
+    if (name === "" || email === "" || mobile === "") {
         alert("Please fill all details.");
         return;
     }
 
-    alert("Registration Successful!");
+    localStorage.setItem("studentName", name);
+    localStorage.setItem("studentEmail", email);
+    localStorage.setItem("studentMobile", mobile);
+
+    window.location.href = "quiz.html";
 
 });
