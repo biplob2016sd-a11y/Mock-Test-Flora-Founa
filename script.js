e443bd85eeb031e074805f26cfb637e2d0174912
biplob2016sd-a11y <biplob2016sd@gmail.com>
2026-07-13 20:56:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+document.getElementById("startBtn").addEventListener("click", function () {
 
+    let name = document.getElementById("name").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let mobile = document.getElementById("mobile").value.trim();
+
+    if(name=="" || email=="" || mobile==""){
+        alert("Please fill all details.");
+        return;
+    }
+
+    alert("Registration Successful!");
+
+});
