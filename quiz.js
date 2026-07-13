ae1b77e45c94e6c4df5d65217c31a30bd307f141
biplob2016sd-a11y <biplob2016sd@gmail.com>
2026-07-13 21:19:52 +0530

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,1 +1,53 @@
+let currentQuestion = 0;
 
+const question = document.getElementById("question");
+const options = document.getElementById("options");
+
+function loadQuestion(){
+
+let q = questions[currentQuestion];
+
+question.innerHTML = (currentQuestion+1)+". "+q.question;
+
+options.innerHTML="";
+
+q.options.forEach((option,index)=>{
+
+options.innerHTML+=`
+<p>
+<label>
+<input type="radio" name="answer">
+${option}
+</label>
+</p>
+`;
+
+});
+
+}
+
+loadQuestion();
+
+document.getElementById("nextBtn").onclick=function(){
+
+if(currentQuestion<questions.length-1){
+
+currentQuestion++;
+
+loadQuestion();
+
+}
+
+}
+
+document.getElementById("prevBtn").onclick=function(){
+
+if(currentQuestion>0){
+
+currentQuestion--;
+
+loadQuestion();
+
+}
+
+}
